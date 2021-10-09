0d86146f10a61fdb7413a0307e43226a7ac6e2c1
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2021-10-10 01:56:44 +0530

    Update script.py

--- script.py
+++ script.py
@@ -21,7 +21,7 @@ Click <i>/help</i> for more details....</b>"""
 NOTE: Download may take some time! So please wait for it to complete!"""
 
 
-    ABOUT_TEXT = """⭕️<b>My Name : TroJanZ Zee5 DL</b>
+    ABOUT_TEXT = """⭕️<b>My Name : KOT Zee5 DL</b>
 
 ⭕️<b>Creater :</b> @KOT_BOTS
 
